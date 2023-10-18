76a23930123d5f0b3c3b83c181c611c46888ea87
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2023-10-18 22:29:24 +0530

    no update image working

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
+import os
 from PIL import Image, ImageDraw
+from flask import Flask, render_template, request
+import json
 
-image = Image.open('templates/1.jpg')
-logo = Image.open('logo/company.jpeg')
+app = Flask(__name__)
 
-logo = logo.resize((80, 80))  # Resize to 80x80 pixels
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-image.paste(logo, (50, 50))  # Paste the logo at the top left corner
+@app.route("/post", methods=['POST'])
+def post():
+    # get data from form
+    form_data = request.get_json()
+    name = form_data['name']
+    card = form_data['card']
+    logo = form_data['logo']
+    print(name, card, logo)
+    # image = Image.open('templates/' + card + '.jpg')
+    # logo = Image.open('logo/' + logo + '.jpeg')
+    # logo = logo.resize((80, 80))  # Resize to 80x80 pixels
+    # image.paste(logo, (50, 50))  # Paste the logo at the top left corner
+    # I1 = ImageDraw.Draw(image)
+    # I1.text((58, 136), name, fill=(0, 0, 0))
+    # image.save('static/' + 'image_with_logo' + '.jpg')
+    image = os.path.join('static', 'image_with_logo.jpg')
+    return render_template('result.html', image=image)
 
-I1 = ImageDraw.Draw(image)
-I1.text((58, 136), "Nice Company", fill=(0, 0, 0))
-
-image.save('results/image_with_logo.jpg')
-
-
+@app.route("/get", methods=['GET'])
+def get():
+    image = os.path.join('static', 'image_with_logo.jpg')
+    return render_template('result.html', image=image)
 
