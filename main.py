0a2587cc54f000ca7663cf6a508cd06aaec1a090
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2023-10-19 00:22:59 +0530

    working python flask

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import io
 import os
 from PIL import Image, ImageDraw
 from flask import Flask, render_template, request
@@ -12,23 +13,17 @@ def index():
 @app.route("/post", methods=['POST'])
 def post():
     # get data from form
-    form_data = request.get_json()
-    name = form_data['name']
-    card = form_data['card']
-    logo = form_data['logo']
+    name = request.form.get('name')
+    card = request.form.get('template')
+    logo = request.files.get('upload-image')
     print(name, card, logo)
-    # image = Image.open('templates/' + card + '.jpg')
-    # logo = Image.open('logo/' + logo + '.jpeg')
-    # logo = logo.resize((80, 80))  # Resize to 80x80 pixels
-    # image.paste(logo, (50, 50))  # Paste the logo at the top left corner
-    # I1 = ImageDraw.Draw(image)
-    # I1.text((58, 136), name, fill=(0, 0, 0))
-    # image.save('static/' + 'image_with_logo' + '.jpg')
-    image = os.path.join('static', 'image_with_logo.jpg')
-    return render_template('result.html', image=image)
-
-@app.route("/get", methods=['GET'])
-def get():
+    image = Image.open('templates/' + card + '.jpg')
+    logo = Image.open(logo)
+    logo = logo.resize((80, 80))  # Resize to 80x80 pixels
+    image.paste(logo, (50, 50))  # Paste the logo at the top left corner
+    I1 = ImageDraw.Draw(image)
+    I1.text((58, 136), name, fill=(0, 0, 0))
+    image.save('static/' + 'image_with_logo' + '.jpg')
     image = os.path.join('static', 'image_with_logo.jpg')
     return render_template('result.html', image=image)
 
